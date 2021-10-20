ccdd8537c197ad62ab7f7095d68e8a305c4b8a2a
Sho Hanakawa <HANAKAWAS@asrl.a-star.edu.sg>
2021-10-20 11:03:53 +0800

    tweak

--- Sho/R/3-output_plots.R
+++ Sho/R/3-output_plots.R
@@ -14,6 +14,10 @@ png("plots/DimPlot_celltype.png", width = 800*2.5, height = 600*3, res = 300)
 DimPlot(hcc, group.by = "celltype", label = TRUE) + NoLegend() 
 dev.off()
 
+png("plots/DimPlot_celltype_sub.png", width = 800*3, height = 600*3.5, res = 200)
+DimPlot(hcc, group.by = "celltype_sub", label = TRUE) + NoLegend() 
+dev.off()
+
 png("plots/DimPlot_tissue.png", width = 800*3, height = 600*3, res = 300)
 DimPlot(hcc, group.by = "tissue") 
 dev.off()
@@ -23,14 +27,35 @@ DimPlot(hcc, split.by = "tissue", group.by = "celltype", ncol = 3) + NoAxes()
 dev.off()
 
 # FeaturePlot
-p <- FeaturePlot(hcc, features = c("CD3D", "NKG7", "CD79A", "MS4A1", "HLA-DRA", "CD74", "CD68", "KIT", "CTSG", "SELL", "TCF4", "MKI67"),
+p <- FeaturePlot(hcc, features = c("CD3D", "NKG7", "CD79A", "MS4A1", "HLA-DRA", "CD74",
+                                   "CD68", "KIT", "CTSG", "SELL", "TCF4", "MKI67"),
                  pt.size = 0.01, min.cutoff = 0, combine = FALSE)
 p <- lapply(X = p,
             FUN = function(p){
               return(p + NoLegend() + NoAxes() + theme(plot.title = element_text(size = 10)))
             })
 png("plots/FeaturePlot.png", width = 800*2, height = 600*2, res = 300)
-print(cowplot::plot_grid(plotlist = p, ncol = 4))
+cowplot::plot_grid(plotlist = p, ncol = 4)
+dev.off()
+
+
+p <- FeaturePlot(hcc, features = c("LYZ", "LAMP3", "SLC40A1", "FCER1A"),
+                 pt.size = 0.01, min.cutoff = 0, combine = FALSE)
+p <- lapply(X = p,
+            FUN = function(p){
+              return(p + NoLegend() + NoAxes() + theme(plot.title = element_text(size = 8)))
+            })
+png("plots/FeaturePlot_targetgenes.png", width = 800*2.5, height = 600*1, res = 300)
+cowplot::plot_grid(plotlist = p, ncol = 4)
+dev.off()
+
+
+png("plots/FeaturePlot_tissue_LAMP3.png", width = 800*5, height = 600*1.5, res = 200)
+FeaturePlot(hcc, features = "LAMP3", split.by = "tissue", pt.size = 1, min.cutoff = 0)
+dev.off()
+
+png("plots/FeaturePlot_tissue_SLC40A1.png", width = 800*5, height = 600*1.5, res = 200)
+FeaturePlot(hcc, features = "SLC40A1", split.by = "tissue", pt.size = 1, min.cutoff = 0)
 dev.off()
 
 
@@ -40,6 +65,10 @@ DotPlot(hcc, features = unique(feature_genes$gene), group.by = "celltype_sub", a
   theme(axis.text.x = element_text(angle = 45, hjust=1))
 dev.off()
 
+png("plots/Dotplot_CellSub_targetgenes.png", width = 800*2.5, height = 600*1, res = 150)
+DotPlot(hcc, features = c("LYZ", "LAMP3", "SLC40A1", "FCER1A"), group.by = "celltype_sub", assay = "RNA") + 
+  coord_flip() + theme(axis.text.x = element_text(angle = 45, hjust=1))
+dev.off()
 
 # Propotion of cell subset in each tissue
 prop <- prop.table(table(hcc$celltype, hcc$tissue), margin = 2) %>% as.data.frame.table
